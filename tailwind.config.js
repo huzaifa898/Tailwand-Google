47e05371c73d49f54cc9f0dbab4c24d2d916f7c7
huzaifa898 <122553707+huzaifa898@users.noreply.github.com>
2023-07-17 15:59:51 +0500

    add coustmize font family to confg.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         'gray': '#8492a6',
         'gray-light': '#d3dce6',
       },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
       extend: {},
     },
     plugins: [],
