76098a7cbadc0094112cd3fe6b4f7371f44cc02d
huzaifa898 <122553707+huzaifa898@users.noreply.github.com>
2023-07-18 16:13:43 +0500

    text deco and opacity concept

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,7 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     content: ["*.{html,js}"],
     theme: {
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
+      
       extend: {},
     },
     plugins: [],
